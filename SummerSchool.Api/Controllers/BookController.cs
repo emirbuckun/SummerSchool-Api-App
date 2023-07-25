947064d462b55cd672657912c541834b9e7ae560
Emir Büçkün <emirbuckun@hotmail.com>
2023-07-25 13:54:12 +0300

    Book controller modified with handler

--- SummerSchool.Api/Controllers/BookController.cs
+++ SummerSchool.Api/Controllers/BookController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using SummerSchool.App.Entity;
+using SummerSchool.App.Handler;
 
 namespace SummerSchool.Api.Controllers
 {
@@ -7,94 +8,56 @@ namespace SummerSchool.Api.Controllers
   [Route("[controller]")]
   public class BookController : ControllerBase
   {
-    private static readonly List<Book> _bookList = new()
-  {
-    new Book(1, "Head First Design Patterns"),
-    new Book(2, "Clean Architecture")
-  };
-
-    public BookController() { }
+    private readonly BookHandler _handler;
+    public BookController()
+    {
+      _handler = new BookHandler();
+    }
 
     [HttpGet]
     [Route("list")]
     public IActionResult Get()
     {
-      return Ok(_bookList);
+      return Ok(_handler.GetBooks());
     }
 
     [HttpGet]
     public IActionResult Get(int id)
     {
-      return Ok(_bookList.SingleOrDefault(x => x.Id == id));
+      return Ok(_handler.GetBook(id));
     }
 
     [HttpPost]
     public IActionResult Post([FromBody] Book request)
     {
-      if (IsValid(request))
-      {
-        if (request is not null && request.Title is not null)
-        {
-          var isExist = _bookList.SingleOrDefault(x => x.Title == request.Title);
-          if (isExist is not null)
-          {
-            _bookList.Add(new Book(_bookList.Count + 1, request.Title));
-            return Ok();
-          }
-          else
-          {
-            return Conflict();
-          }
-        }
-        else
-        {
-          return Conflict();
-        }
-      }
+      var result = _handler.AddBook(request);
+
+      if (!result)
+        return NotFound();
       else
-      {
-        return BadRequest();
-      }
+        return Ok();
     }
 
     [HttpPut]
     public IActionResult Put([FromBody] Book request)
     {
-      var existingBook = _bookList.SingleOrDefault(x => x.Id == request.Id);
-      if (existingBook is not null)
-      {
-        existingBook = request;
-        return Ok();
-      }
-      else
-      {
+      var result = _handler.UpdateBook(request);
+
+      if (!result)
         return NotFound();
-      }
+      else
+        return Ok();
     }
 
     [HttpDelete]
     public IActionResult Delete(int id)
     {
-      Book? book = _bookList.SingleOrDefault(x => x.Id == id);
+      var result = _handler.DeleteBook(id);
 
-      if (book is not null)
-      {
-        _bookList.Remove(book);
-        return Ok();
-      }
-      return NotFound();
-    }
-
-    private static bool IsValid(Book book)
-    {
-      if (string.IsNullOrEmpty(book.Title))
-      {
-        return false;
-      }
+      if (!result)
+        return NotFound();
       else
-      {
-        return true;
-      }
+        return Ok();
     }
   }
 }
