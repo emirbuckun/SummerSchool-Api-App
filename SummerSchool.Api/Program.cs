4967b4d43ee7863cbbaa8dff85cf56da8f761416
Emir Büçkün <emirbuckun@hotmail.com>
2023-07-25 15:30:23 +0300

    BookStore renamed as BookRepository

--- SummerSchool.Api/Program.cs
+++ SummerSchool.Api/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddScoped(typeof(BookHandler));
-builder.Services.AddSingleton(typeof(BookStore));
+builder.Services.AddSingleton(typeof(BookRepository));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
